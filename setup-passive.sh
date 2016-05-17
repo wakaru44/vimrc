a8131ee5616d29889a29fa99ab1bcd7f36cd049d
Ubuntu <ubuntu@ip-10-91-165-69.eu-west-1.compute.internal>
2016-05-17 17:04:34 +0000

    adding passive setup - does not require a user to press enter

---
+++ setup-passive.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function go_home()
+{
+  vihome="$HOME/.vim"
+  if [ ! -d "$vihome" ]; then
+    # Exit if there is no .vim at home
+    echo "Are yo mad bro!? clone the repo properly man"
+    exit 255
+  fi
+  cd $vihome
+}
+
+
+function backup()
+{
+  echo "I'm doing a backup, just in case."
+  echo "It will be located in ~/vim.backup and ~/vimrc.backup"
+  go_home
+  mv .vim vim.backup
+  mv .vimrc vim.backup/vimrc
+}
+
+function clean()
+{
+  go_home
+  rm ../.vimrc
+  # clean the old folders
+  rm -rf bundle/*
+}
+
+
+function setup()
+{
+  go_home
+  # clone the repos
+  cd bundle
+  for repo in $(cat ../plugins.list)
+    do git clone $repo
+  done
+  cd ..
+
+  # use the repo's vimrc
+  ln -s .vim/template.vimrc  ../.vimrc
+}
+
+go_home
+backup
+clean
+setup
+
